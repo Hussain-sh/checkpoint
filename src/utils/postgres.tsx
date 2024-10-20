5887ad357021d46773a4e4bd1c010079ba45bdf3
Hussain <sheikhhussein786@gmail.com>
2024-10-20 19:50:12 +0530

    connect to remote db

--- src/utils/postgres.tsx
+++ src/utils/postgres.tsx
@@ -1,11 +1,21 @@
 import { Pool } from "pg";
 
 const pool = new Pool({
-	host: "localhost",
+	host: "dpg-csagc8rtq21c73911380-a.oregon-postgres.render.com",
 	port: 5432,
-	user: "postgres",
-	password: "Hussain123",
+	user: "postres",
+	password: "yDZ5g9ZGI0Bnq56DwAw4tzsGfKzhxe49",
 	database: "checkpoint_db",
 });
+
+pool
+	.connect()
+	.then((client) => {
+		console.log("Connected to the database!");
+		client.release();
+	})
+	.catch((err) => {
+		console.error("Connection error", err.stack);
+	});
 
 export default pool;
