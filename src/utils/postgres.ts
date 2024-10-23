bf354b709d960e8441e4ced1d72b90dc727d333b
Hussain-sh <87571408+Hussain-sh@users.noreply.github.com>
2024-10-23 09:46:19 +0530

    Update postgres.ts

--- src/utils/postgres.ts
+++ src/utils/postgres.ts
@@ -1,10 +1,10 @@
 import { Pool } from "pg";
 const pool = new Pool({
-	host: process.env.DB_HOST,
-	port: process.env.DB_PORT ? Number(process.env.DB_PORT) : undefined,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_DATABASE,
+	host: "pg-3c0d397f-checkpoint-first.g.aivencloud.com",
+	port: 13919,
+	user: "avnadmin",
+	password: "AVNS_6sPCnn6xGMyPU0UHybN",
+	database: "checkpoint_db",
 	ssl: {
 		rejectUnauthorized: false,
 	},
